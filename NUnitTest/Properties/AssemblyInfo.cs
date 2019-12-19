e63aadaaa9eb63f48215965097129610aa97901a
swapnil29ahirrao <sahirrao@xpanxion.com>
2019-12-19 12:51:35 +0530

    First commit

---
+++ NUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03832de9-1d17-4e52-81f7-f9d28519dc4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NUnitTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestDataBug()
+        {
+            Assert.Fail("Test Data issue");
+        }
+        [Test]
+        public void InvalidParameterException()
+        {
+            Assert.Fail("Invalid Parameter passed");
+        }
+        [Test]
+        public void ElementNotFoundException()
+        {
+            Assert.Fail("Element not found");
+        }
+        [Test]
+        public void newTestMethod()
+        {
+            Assert.Fail("Invalid Parameter passed");
+        }
+    }
+}
